23dd191d9d5d1431324e353ae38a0ed101bacabe
skipjac <skip@skipjack.info>
2013-10-24 14:27:42 -0700

    removed url encoding for body

--- app.js
+++ app.js
@@ -2,25 +2,17 @@
 (function(){
   return {
     appID: 'createTicketApp',
-    defaultState: 'layout',
-    name: '',
-    groups: [],
     targetSpoke: this.$('#zendeskSelect').val() || '',
-    newRequester: this.$('#userName').val() || '',
-    newEmail: this.$('#userEmail').val() || '',
-    newSub: this.$('#userSub').val() || '',
-    newDesc: this.$('#ticketDesc').val() || '',
     events: {
       'click .submitSpoke': 'createTicketValues',
       'createTicket.done': 'processData',
       'click .displayForm': 'switchToReqester'
     }, //end events
     requests: {
-      createTicket: function() {
+      createTicket: function(data) {
         return {
-          url: 'http://' + this.targetSpoke + '/requests/embedded/create.json?subject=' + this.newSub + '&description='+ this.newDesc + '&name=' + this.newRequester  + '&email='+ encodeURI(this.newEmail) + '',
-          dataType: 'JSON',
-          proxy_v2: true
+          url: data,
+          dataType: 'JSON'
         };
       }
     }, //end requests
@@ -33,11 +25,12 @@
     },
     createTicketValues: function() {
       this.targetSpoke = this.$('#zendeskSelect').val();
-      this.newRequester = this.$('#userName').val();
-      this.newEmail = this.$('#userEmail').val();
-      this.newSub = this.$('#userSub').val();
-      this.newDesc = this.$('#ticketDesc').val();
-      this.ajax('createTicket');
+      var spoke = 'http://'+ this.$('#zendeskSelect').val()+'/requests/embedded/create.json?';
+        spoke += 'subject='+ this.$('#userSub').val()+'&';
+        spoke += 'description='+this.$('#ticketDesc').val()+'&';
+        spoke += 'name='+this.$('#userName').val()+'&';
+        spoke += 'email='+ encodeURI(this.$('#userEmail').val())+'&';
+      this.ajax('createTicket', spoke);
     },
     switchToReqester: function(data) {
       this.switchTo('requester', {});
